dfbfdd80bf8bdee4446dfc806e12bb152085e266
Nitesh Isave <nitesh.isave@infinx.com>
2022-03-21 23:08:10 +0530

    Initial Commit

---
+++ MVVM-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Example
+//
+//  Created by Nitesh on 21/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
